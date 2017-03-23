c90bb78736c1161a6e1d214d17b126e5232ba975
Eoin Bailey <baileye@gmail.com>
2017-03-23 16:10:34 +0000

    Create snapshot.ps1

---
+++ snapshot.ps1
@@ -0,0 +1,15 @@
+# COPY ALL FILES TO BLOB STORAGE
+# TO CREATE A SNAPSHOT
+
+# log file
+$logfile = 'C:\code\azcopy.log'
+
+# source
+$sourceDir = 'C:\code\azcopy'
+
+# destination storage account
+#destination storage account  (e.g. https://mydestination.blob.core.windows.net)
+$destkey = "<storage account key>"
+$dest = "<destination storage account>"
+
+azcopy /Source:$sourceDir /S /Dest:$dest /DestKey:$destkey /V:$logfile /Y /MT
